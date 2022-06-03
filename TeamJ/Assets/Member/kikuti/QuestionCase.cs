47c47792f6af97e8436d1778ad1c884466277f58
kikuchitoya <21c1020024kt@edu.tech.ac.jp>
2022-06-04 07:16:49 +0900

    easy,normal,hardの難易度３つをenumを用いて列挙型として作ったQuestionCaseスクリプトを作成。難易度ごとの質問の管理等の部分ができていない。

---
+++ TeamJ/Assets/Member/kikuti/QuestionCase.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestionCase
+{
+    public enum Difficulty
+    {
+        easy,
+        normal,
+        hard
+    }
+
+    Difficulty question1 = Difficulty.easy;
+
+}
